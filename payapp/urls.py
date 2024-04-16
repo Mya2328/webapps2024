ed3808bb01b7ee7e7209c9e2074c3ae938106d55
Mya2328 <myathuzarlwin01@gmail.com>
2024-04-16 17:36:10 +0100

    Adding Project Webapps

---
+++ payapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('webapps2024/', views.home, name='home'),
+    path('summary/', views.summary, name="dashboard"),
+    path('generate_plot/', views.generate_plot, name='generate_plot'),
+]
